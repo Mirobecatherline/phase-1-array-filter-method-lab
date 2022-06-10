b52676c3c911ee870092655fc94aeefd8645a5d2
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-11 00:27:48 +0300

    commit tree 578aa8144e34eb93066648ddd1683e641ded796c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers,name="Sally") {
+ const newlist=drivers.filter(function (names) {
+     return names.toLowerCase()===name.toLowerCase();
+ })  
+ 
+ return newlist; 
+ 
+}
+function fuzzyMatch(drivers,letter="Sa") {
+    const filterstring=drivers.filter(element=>element.toLowerCase().indexOf(letter.toLowerCase())===0)
+    return filterstring;
+}
+
+const drivers1= [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(drivers1,drivername="Sally") {
+     const property=drivers1.filter(fil=>fil.name===drivername)
+     return property;
+}
